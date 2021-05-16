befa590c47df91cb43c39ff26b1dbd4b3c10d4bf
David Castro <51448763+DavidCastroUFC@users.noreply.github.com>
2021-05-16 19:42:11 -0300

    Updates

--- app/src/main/java/br/com/davidcastro/progressbutton/MainActivity.kt
+++ app/src/main/java/br/com/davidcastro/progressbutton/MainActivity.kt
@@ -14,27 +14,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Referenciando os elementos a serem manipulados
+//      //Referencing the elements to be manipulated
         val btn = this.findViewById<androidx.cardview.widget.CardView>(R.id.btnProgress)
         val bar = btn.findViewById<ProgressBar>(R.id.progressBar)
+        val toVibrate = getSystemService(VIBRATOR_SERVICE) as Vibrator
 
-        btn.setOnClickListener {
-            Toast.makeText(applicationContext, "Pressione e segure para aceitar ", Toast.LENGTH_SHORT).show()
-        }
-        
         //adicionando evento de clique longo ao botão
+        // adding a long click event to the button
         btn.setOnLongClickListener {
             object : CountDownTimer(2000, 1) {
                 override fun onTick(millisUntilFinished: Long) {
+                    //Essa condição verificará se o botão ainda está sendo pressionado
+                    // This condition will check if the button is still being pressed
                     if (!btn.isPressed){
                         bar.progress = 0
+                        toVibrate.cancel()
+                        //Cancelar a contagem | Cancel the count
+                        cancel()
                     }else{
                         bar.progress = 1 + bar.progress
+                        toVibrate.vibrate(millisUntilFinished)
                     }
                 }
                 override fun onFinish() {
-                    val vibrar = getSystemService(VIBRATOR_SERVICE) as Vibrator
-                    val milliseconds: Long = 1000
-                    vibrar.vibrate(milliseconds)
+                    //TODO("Action after finish | Ação ao finalizar")
+                    bar.progress = 0
+                    Toast.makeText(applicationContext, "Done!", Toast.LENGTH_SHORT).show()
                 }
             }.start()
             true
